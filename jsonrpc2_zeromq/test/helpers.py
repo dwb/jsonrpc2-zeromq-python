df0e1b981c2e1f24244b5b5de1f8fd78dab69c2c
Dan Brown <dan.brown@wi-ltd.net>
2012-01-19 10:29:39 +0000

    Added missing copyright notice

--- jsonrpc2_zeromq/test/helpers.py
+++ jsonrpc2_zeromq/test/helpers.py
@@ -1,3 +1,8 @@
+# Part of the jsonrpc2-zeromq-python project.
+# (c) 2012 Wireless Innovation Ltd, All Rights Reserved.
+# Please see the LICENSE file in the root of this project for license
+# information.
+
 from time import sleep
 import threading
 
